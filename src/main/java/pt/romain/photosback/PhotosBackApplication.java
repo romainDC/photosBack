cc5026b385792c32e6ebd96ebbf097418c938628
Romain De Carvalho <33512119+romainDC@users.noreply.github.com>
2025-07-02 10:08:40 +0200

    Init
    Signed-off-by: Romain De Carvalho <33512119+romainDC@users.noreply.github.com>
    
    pom.xml
    
    Signed-off-by: Romain De Carvalho <33512119+romainDC@users.noreply.github.com>
    
    application.properties
    
    Signed-off-by: Romain De Carvalho <33512119+romainDC@users.noreply.github.com>

---
+++ src/main/java/pt/romain/photosback/PhotosBackApplication.java
@@ -0,0 +1,15 @@
+package pt.romain.photosback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosBackApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PhotosBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/romain/photosback/PhotosBackApplicationTests.java
@@ -0,0 +1,18 @@
+package pt.romain.photosback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotosBackApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+        // Le corps de la méthode est vide car l'objectif est simplement
+        // de vérifier que le contexte Spring Boot se charge sans erreur.
+        // Si le contexte se charge, le test passe. Sinon, il échoue.
+    }
+
+}
