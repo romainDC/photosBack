6bb0ecc219688103faba89672c430de1d12a5eed
Romain De Carvalho <33512119+romainDC@users.noreply.github.com>
2025-07-08 09:13:09 +0200

    Ajout TU enregistrement user
    Signed-off-by: Romain De Carvalho <33512119+romainDC@users.noreply.github.com>

--- src/test/java/pt/romain/photosback/service/UserServiceTests.java
+++ src/test/java/pt/romain/photosback/service/UserServiceTests.java
@@ -1,53 +1,112 @@
 package pt.romain.photosback.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import pt.romain.photosback.dto.UserDto;
 import pt.romain.photosback.entities.User;
+import pt.romain.photosback.repository.UserRepository;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceTests
 {
-    private static final String EXPECTED_LOGIN = "testLogin";
-    private static final String EXPECTED_PASSWORD = "testPassword";
-    private static final String EXPECTED_MAIL = "testMail";
+    private static final String LOGIN = "testLogin";
+    private static final String PASSWORD = "testPassword";
+    private static final String MAIL = "testMail";
 
-    @Autowired
-    private UserService userService;
+    private static UserRepository userRepositoryMock;
+    private static UserService userServiceMock;
+
+    @BeforeAll
+    static void setUpTests()
+    {
+        userRepositoryMock = mock(UserRepository.class);
+        PasswordEncoder passwordEncoderMock = mock(PasswordEncoder.class);
+        TeamService teamServiceMock = mock(TeamService.class);
+        userServiceMock = new UserServiceImpl(passwordEncoderMock, teamServiceMock, userRepositoryMock);
+    }
 
     @Test
     void testConvertEntityToDto()
     {
-        final User user = User.builder().login(EXPECTED_LOGIN).password(EXPECTED_PASSWORD).mail(EXPECTED_MAIL).build();
-        final UserDto actualUserDto = userService.convertEntityToDto(user);
+        final User user = User.builder().login(LOGIN).password(PASSWORD).mail(MAIL).build();
+        final UserDto actualUserDto = userServiceMock.convertEntityToDto(user);
         Assertions.assertNotNull(actualUserDto);
-        Assertions.assertEquals(EXPECTED_LOGIN, actualUserDto.login());
-        Assertions.assertEquals(EXPECTED_PASSWORD, actualUserDto.password());
-        Assertions.assertEquals(EXPECTED_MAIL, actualUserDto.mail());
+        Assertions.assertEquals(LOGIN, actualUserDto.login());
+        Assertions.assertEquals(PASSWORD, actualUserDto.password());
+        Assertions.assertEquals(MAIL, actualUserDto.mail());
     }
 
     @Test
     void testConvertDtoToEntity()
     {
-        final UserDto userDto = UserDto.builder().login(EXPECTED_LOGIN).password(EXPECTED_PASSWORD).mail(EXPECTED_MAIL).build();
-        final User actualUser = userService.convertDtoToEntity(userDto);
+        final UserDto userDto = UserDto.builder().login(LOGIN).password(PASSWORD).mail(MAIL).build();
+        final User actualUser = userServiceMock.convertDtoToEntity(userDto);
         Assertions.assertNotNull(actualUser);
-        Assertions.assertEquals(EXPECTED_LOGIN, actualUser.getLogin());
-        Assertions.assertEquals(EXPECTED_PASSWORD, actualUser.getPassword());
-        Assertions.assertEquals(EXPECTED_MAIL, actualUser.getMail());
+        Assertions.assertEquals(LOGIN, actualUser.getLogin());
+        Assertions.assertEquals(PASSWORD, actualUser.getPassword());
+        Assertions.assertEquals(MAIL, actualUser.getMail());
     }
 
     @Test
     void testConvertEntityToDtoNull()
     {
-        Assertions.assertNull(userService.convertEntityToDto(null));
+        Assertions.assertNull(userServiceMock.convertEntityToDto(null));
     }
 
     @Test
     void testConvertDtoToEntityNull()
     {
-        Assertions.assertNull(userService.convertDtoToEntity(null));
+        Assertions.assertNull(userServiceMock.convertDtoToEntity(null));
+    }
+
+    @Test
+    void testRegisterNewUserNull()
+    {
+        Assertions.assertNull(userServiceMock.registerNewUser(null));
+    }
+
+    @Test
+    void testRegisterNewUserLogin()
+    {
+        final UserDto userDto = UserDto.builder().login(LOGIN).password(PASSWORD).mail(MAIL).build();
+        when(userRepositoryMock.findByLogin(LOGIN)).thenReturn(Optional.of(new User()));
+
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> userServiceMock.registerNewUser(userDto), "Username already taken.");
+    }
+
+    @Test
+    void testRegisterNewUserMail()
+    {
+        final UserDto userDto = UserDto.builder().login(LOGIN).password(PASSWORD).mail(MAIL).build();
+        when(userRepositoryMock.findByLogin(LOGIN)).thenReturn(Optional.empty());
+        when(userRepositoryMock.findByMail(MAIL)).thenReturn(Optional.of(new User()));
+
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> userServiceMock.registerNewUser(userDto), "Email already registered.");
+    }
+
+    @Test
+    void testRegisterNewUser()
+    {
+        final UserDto userDto = UserDto.builder().login(LOGIN).password(PASSWORD).mail(MAIL).build();
+        when(userRepositoryMock.findByLogin(LOGIN)).thenReturn(Optional.empty());
+        when(userRepositoryMock.findByMail(MAIL)).thenReturn(Optional.empty());
+        when(userRepositoryMock.save(Mockito.any(User.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0)); // Returns the first argument passed to save()
+
+        UserDto actualUserDto = userServiceMock.registerNewUser(userDto);
+
+        verify(userRepositoryMock, times(1)).save(Mockito.any(User.class));
+        Assertions.assertNotNull(actualUserDto);
+        Assertions.assertEquals(LOGIN, actualUserDto.login());
+        Assertions.assertNotEquals(PASSWORD, actualUserDto.password());
+        Assertions.assertEquals(MAIL, actualUserDto.mail());
     }
 }
